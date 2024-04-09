b0bf390942c83d70d09b9eaf15e1b28b4e211bc5
YosieSYX <141898763+YosieSYX@users.noreply.github.com>
2024-04-09 11:17:08 +0800

    Update Stack_array implementation.h

--- Stack/Stack_array implementation.h
+++ Stack/Stack_array implementation.h
@@ -1,1 +1,40 @@
+#ifndef STACK
+#define STACK
 
+template <class T> 
+class Stack
+{
+private: 
+  int _size;
+  T *array;
+public:
+  Stack(){
+    _size = 0;
+    array = new int [100];
+  }
+  bool isEmpty()
+  {
+    if(_size == 0)
+    {
+      return true;
+    }
+    return false;
+  }
+  
+  void push(T item)
+  {
+    array[_size] = item;
+    _size += 1;
+  }
+  void pop()
+  {
+    if(isEmpty())
+    {
+      throw :: std :: out_of_range("no element");
+    }
+    else
+    {
+      _size -= 1;
+    }
+  }
+};
